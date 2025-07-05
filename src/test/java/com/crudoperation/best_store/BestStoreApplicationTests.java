46248a23e72509b105b8b6eff685d06ed73313d8
hamedragaab22 <hamedragab538@gmail.com>
2025-07-05 19:18:37 +0300

    initial commit

---
+++ src/test/java/com/crudoperation/best_store/BestStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudoperation.best_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
