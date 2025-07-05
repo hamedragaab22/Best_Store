eb0719c598dfbdbbecdee24a275043b1da76cb22
hamedragaab22 <hamedragab538@gmail.com>
2025-07-05 23:22:08 +0300

    craete the endpoitn return list of products and it's file of html

---
+++ src/main/java/com/crudoperation/best_store/controllers/ProductsController.java
@@ -0,0 +1,24 @@
+package com.crudoperation.best_store.controllers;
+
+import com.crudoperation.best_store.models.Product;
+import com.crudoperation.best_store.services.ProductsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/products")
+public class ProductsController {
+    @Autowired
+    private ProductsRepository repo;
+    @GetMapping({"","/"})
+    public String showProductList(Model model){
+        List<Product>products=repo.findAll();
+        model.addAttribute("products",products);
+        return "products/index";
+    }
+}
